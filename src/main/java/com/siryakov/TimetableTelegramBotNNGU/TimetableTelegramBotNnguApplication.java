588ca779a35cff34c877fa8e362595e78d5d603b
Kirill Siryakov <SiryakovJob@gmail.com>
2023-08-30 20:38:17 +0300

    Add SpringBoot skeleton to the project

---
+++ src/main/java/com/siryakov/TimetableTelegramBotNNGU/TimetableTelegramBotNnguApplication.java
@@ -0,0 +1,13 @@
+package com.siryakov.TimetableTelegramBotNNGU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableTelegramBotNnguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableTelegramBotNnguApplication.class, args);
+	}
+
+}
